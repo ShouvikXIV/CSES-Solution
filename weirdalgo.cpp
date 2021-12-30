214917afa96f71f5721d0b6bd039034c8c919151
Shouvik Barua <85792552+ShouvikXIV@users.noreply.github.com>
2021-12-30 18:30:23 +0600

    Add files via upload

---
+++ weirdalgo.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long int n;
+    cin>>n;
+    while(n!=1)
+    {
+        cout<<n<<' ';
+        if(n%2==0)
+        {
+            n = n/2;
+        }
+        else
+        {
+            n = (n*3)+1;
+        }
+    }
+    cout<<1;
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
